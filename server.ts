2b678b60902e7d158292599f3604a08b93d9d0c2
Keith Francis Balde <keithfrancisb@gmail.com>
2021-08-24 21:50:05 -0400

    remove express. add initial cors

--- server.ts
+++ server.ts
@@ -1,23 +1,20 @@
-import express from 'express';
-import { ApolloServer } from 'apollo-server-express';
+import { ApolloServer } from 'apollo-server';
 import typeDefs from './src/schema/typeDefs';
 import resolvers from './src/schema/resolvers';
-import http from 'http';
 
 const PORT = process.env.PORT || 8081;
 
 async function startApolloServer() {
-  const app = express();
-
   const server = new ApolloServer({
+    cors: {
+      origin: '*',
+      credentials: true
+    },
     typeDefs,
     resolvers,
   });
-  await server.start();
-  server.applyMiddleware({ app });
   
-  const httpServer = http.createServer(app);
-  httpServer.listen({port: PORT}, (): void => {
+  server.listen({port: PORT}, (): void => {
     console.log(`🚀GraphQL-Server is running on http://localhost:${PORT}/graphql`);
   });
 }
