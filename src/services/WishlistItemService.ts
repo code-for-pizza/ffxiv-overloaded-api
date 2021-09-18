f24a28d5fd4df2d2fa8995bcb3f6a5340d5dc94a
Keith Francis Balde <keithfrancisb@gmail.com>
2021-09-18 22:56:35 +0000

    fix indentation of sql string for getWishlistItems

--- src/services/WishlistItemService.ts
+++ src/services/WishlistItemService.ts
@@ -9,12 +9,12 @@ export const getWishlistItemsByUserID = async (
   userID: number
 ): Promise<number[]> => {
   const sql = `
-      SELECT
-        item_id
-      FROM tbl_wishlisted_items
-      WHERE
-        user_id = ?
-    `;
+    SELECT
+      item_id
+    FROM tbl_wishlisted_items
+    WHERE
+      user_id = ?
+  `;
 
   const result = await knex.raw(sql, [userID]);
 
